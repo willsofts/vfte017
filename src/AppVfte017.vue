f111dde629173b6edfa00ceaf93148b3e7af5f49
tassun <32536248+tassun@users.noreply.github.com>
2024-10-28 10:00:47 +0700

    support build version

--- src/AppVfte017.vue
+++ src/AppVfte017.vue
@@ -2,7 +2,7 @@
 <template>
   <div id="fswaitlayer" class="fa fa-spinner fa-spin"></div>
   <div class="pt-page pt-page-current pt-page-controller search-pager">
-    <PageHeader ref="pageHeader" :labels="labels" pid="vfte017" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" />
+    <PageHeader ref="pageHeader" :labels="labels" pid="vfte017" version="1.0.0" showLanguage="true" @language-changed="changeLanguage" :multiLanguages="multiLanguages" :build="buildVersion" />
     <SearchForm ref="searchForm" :labels="labels" :dataCategory="dataCategory" @data-select="dataSelected" />
   </div>
   <teleport to="#modaldialog">
@@ -18,6 +18,7 @@ import { getLabelModel, getMultiLanguagesModel } from "@willsofts/will-app";
 import { getDefaultLanguage, setDefaultLanguage } from "@willsofts/will-app";
 import { startApplication } from "@willsofts/will-app";
 
+const buildVersion = process.env.VUE_APP_BUILD_DATETIME;
 export default {
   components: {
     PageHeader, SearchForm, EntryForm
@@ -26,7 +27,7 @@ export default {
     const dataCategory = { };
     let labels = ref(getLabelModel());
     const multiLanguages = ref(getMultiLanguagesModel());
-    return { multiLanguages, labels, dataCategory };
+    return { buildVersion, multiLanguages, labels, dataCategory };
   },
   mounted() {
     console.log("App: mounted ...");
