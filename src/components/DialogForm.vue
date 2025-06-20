37456c3d6fa5bb699df29b367a0d44232649c9b8
tassan_oro <32536248+tassun@users.noreply.github.com>
2025-06-20 16:53:40 +0700

    support language change from menu

--- src/components/DialogForm.vue
+++ src/components/DialogForm.vue
@@ -1,10 +1,10 @@
 <template>
 <div :id="id" class="modal fade pt-page pt-page-item" tabindex="-1" role="dialog" v-bind="$attrs">
-    <div class="modal-dialog modal-lg">
+    <div class="modal-dialog modal-xm">
         <div class="modal-content portal-area fa-portal-area">
             <div class="modal-header">
                 <slot name="header"></slot>
-                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <button type="button" class="close btn-closing" data-dismiss="modal"></button>
             </div>
             <div class="entry-dialog-layer">
                 <slot></slot>
